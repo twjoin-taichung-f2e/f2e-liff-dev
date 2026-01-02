99a0f7abcc18ca65c4ffbd069d068e8d76948817
Nelson <nelson@twjoin.com>
2026-01-02 17:56:26 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import liff from "@line/liff";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 
 const name = ref<string>("");
 const error = ref<any>(null);
@@ -12,24 +12,33 @@ const liffId = runtimeConfig.public.LIFF_ID;
 // Init LIFF when DOM is mounted
 // https://vuejs.org/api/composition-api-lifecycle.html#onmounted
 onMounted(async () => {
+  console.log('liffId:',liffId); // TODO: Will delete
+  
   if (!liffId) {
     console.error("Please set LIFF_ID in .env file");
     return;
   }
 
-  await liff.init({ liffId: liffId });
-  console.log("LIFF init success");
-  console.log("LIFF SDK version", liff.getVersion());
+  try {
+    await liff.init({ liffId: liffId });
+    console.log("LIFF init success");
+    console.log("LIFF SDK version", liff.getVersion());
 
-  liff
-    .getProfile()
-    .then((profile) => {
-      name.value = profile.displayName;
-    })
-    .catch((err) => {
-      error.value = err;
-      console.log("error", err);
-    });
+    // 檢查使用者是否已登入
+    if (!liff.isLoggedIn()) {
+      // 如果未登入，導向登入頁面
+      liff.login();
+      return;
+    }
+
+    // 取得使用者資訊
+    const profile = await liff.getProfile();
+    name.value = profile.displayName;
+    console.log("User profile:", profile);
+  } catch (err) {
+    error.value = err;
+    console.error("LIFF error:", err);
+  }
 });
 </script>
 
